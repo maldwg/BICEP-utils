e61d54fbc7464c57feb9ab7ca547de74a612dce1
maldwg <maxludwig3@t-online.de>
2024-06-11 13:24:48 +0200

    update utils to work with new backend design

--- fastapi/routes.py
+++ fastapi/routes.py
@@ -15,11 +15,16 @@ router = APIRouter()
 async def healthcheck():
     return {"message": "healthy"}
 
+# TODO: einmal alles testen (setup container, ensemble, analysis start, stop, etc.)
+
 @router.post("/configuration")
-async def test(file: UploadFile = None ,ids: IDSBase = Depends(get_ids_instance)):
+async def test(container_id: str = Form(...) , file: UploadFile = Form(...)  ,ids: IDSBase = Depends(get_ids_instance)):
     if file is None:
         raise HTTPException(status_code=400, detail="No file provided")
     
+    # initialize container id variable to keep track which container is associated with the ids instance
+    ids.container_id = int(container_id)
+
     temporary_file_path = "/tmp/temporary.txt"
     await save_file(file, temporary_file_path)
     response = await ids.configure(temporary_file_path)
@@ -49,22 +54,42 @@ async def static_analysis(ensemble_id: Optional[str] = Form(None), container_id:
     await save_file(file, temporary_file_path)
     asyncio.create_task(ids.startStaticAnalysis(temporary_file_path, int(container_id)))
     http_response = Response(content=f"Started analysis for container {container_id}", status_code=200)
+
     return http_response
 
 @router.post("/analysis/network")
 async def network_analysis(network_analysis_data: NetworkAnalysisData, ids: IDSBase = Depends(get_ids_instance)):
+    if network_analysis_data.ensemble_id != None:
+        ids.ensemble_id = network_analysis_data.ensemble_id
+
     response = await ids.startNetworkAnalysis(network_analysis_data.container_id)
     return response
 
 @router.post("/analysis/stop/")
 async def stop_analysis(ids: IDSBase = Depends(get_ids_instance)):
-    response = await ids.stopAnalysis()    
+    response = await ids.stopAnalysis()  
+
+    # reset ensemble id to wait if next analysis is for ensemble or ids solo
+    if ids.ensemble_id != None:
+        ids.ensemble_id = None
+  
     return response
 
-async def tell_core_analysis_has_finished(container_id: int):
+async def tell_core_analysis_has_finished(ids: IDSBase = Depends(get_ids_instance)):
+    if ids.ensemble_id == None:
+        endpoint = f"/ids/analysis/finished/{ids.container_id}"
+    else:
+        endpoint = f"/ensemble/{ids.ensemble_id}/analysis/finished/{ids.container_id}"
+    
     # tell the core to stop/set status to idle again
     core_url = await get_env_variable("CORE_URL")
-    endpoint = f"/ids/analysis/finished/{container_id}"
+        # reset ensemble id to wait if next analysis is for ensemble or ids solo
+
     async with httpx.AsyncClient() as client:
         response: HTTPResponse = await client.post(core_url+endpoint)
+
+    # reset ensemble id after each analysis is completed to keep track if analysis has been triggered for ensemble or not
+    if ids.ensemble_id != None:
+        ids.ensemble_id = None
+
     return response
--- models/ids_base.py
+++ models/ids_base.py
@@ -1,38 +1,32 @@
 from abc import ABC, abstractmethod
 
+
 class IDSBase(ABC):
     """
     Abstract base class for all IDS supported by BICEP
     Each IDS involved needs to inherit from this base class and implement the following methods nad attributes
     """
     
+    container_id: int = None
+    ensemble_id: int = None
+    
     @property
     @abstractmethod
-    def log_location(self):
+    async def log_location(self):
         pass
 
     @property
     @abstractmethod
-    def network_interface(self):
+    async def network_interface(self):
         pass
 
     @property
     @abstractmethod
-    def configuration_location(self):
-        pass
-
-    @property
-    @abstractmethod
-    def container_id(self):
-        pass
-
-    @property
-    @abstractmethod
-    def ensemble_id(self):
+    async def configuration_location(self):
         pass
 
     @abstractmethod
-    def configure(self, file_path):
+    async def configure(self, file_path):
         """
         Method for setting up the main configuration file in the corresponding location
         gets a file content as input and needs to save it to the location necesary for the IDS system
@@ -40,7 +34,7 @@ class IDSBase(ABC):
         return "base implementation"
 
     @abstractmethod
-    def configure_ruleset(self, file_path):
+    async def configure_ruleset(self, file_path):
         """
         Method for setting up the main configuration file in the corresponding location
         gets a file content as input and needs to save it to the location necesary for the IDS system
@@ -49,7 +43,7 @@ class IDSBase(ABC):
 
 
     @abstractmethod
-    def startStaticAnalysis(self, file_path: str, container_id: int):
+    async def startStaticAnalysis(self, file_path: str, container_id: int):
         """
         Method to trigger a static analysis. This method takes a file as input and executes certain OS commands to trigger the analysis
         """
@@ -57,20 +51,21 @@ class IDSBase(ABC):
 
         
     @abstractmethod
-    def startNetworkAnalysis(self):
+    async def startNetworkAnalysis(self):
         """
         Method to start the network analysis mode where the traffic is caputred on an internal network interface
         """
         pass
 
 
-        
-    @abstractmethod
-    def stopAnalysis(self):
-        """
-        Stop ongoing analysis' by interrputing the process
-        """
-        pass
+    async def stopAnalysis(self):
+        from src.utils.fastapi.utils import stop_process
+        from src.utils.fastapi.routes import tell_core_analysis_has_finished
+
+        await stop_process(self.pid)
+        self.pid = None
+        await tell_core_analysis_has_finished()
+
 
         
     # def sendMetrics(self):
@@ -78,9 +73,4 @@ class IDSBase(ABC):
 
     
     # def sendAlerts(self):
-    #     pass
-
-        
-    # @abstractmethod
-    # def configure(self):
     #     pass
